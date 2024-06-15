2df33ed69bc7bf3eb10a03d30792eb8237af59a2
nikenhpsr <niken18003@mail.unpad.ac.id>
2024-06-15 21:39:13 +0700

    fix: netlify config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
 export default defineConfig({
   plugins: [
     remix({
       ignoredRouteFiles: ["**/*.css"],
     }),
+    netlifyPlugin(),
     tsconfigPaths(),
   ],
 });
